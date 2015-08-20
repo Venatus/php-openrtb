3a8a00153fc6feb30bbd969d30e7c784deb924c9
Chris Beck <cwbeck@gmail.com>
2015-08-20 15:28:39 +0000

    updates

--- src/openrtb/BidResponse.php
+++ src/openrtb/BidResponse.php
@@ -3,5 +3,31 @@ namespace openrtb;
 
 class BidResponse {
   
+  protected $attributes = array(
+    'id' => array(
+      'required' => true,
+      'type' => 'string',
+    ),
+    'seatbid' => array(
+      'required' => true,
+      'type' => 'models/SeatBid',
+    ),
+    'bidid' => array(
+      'type' => 'string',
+    ),
+    'cur' => array(
+      'type' => 'string',
+      'default_value' => 'USD',
+    ),
+    'customdata' => array(
+      'type' => 'string',
+    ),
+    'nbr' => array(
+      'type' => 'integer',
+    ),
+    'ext' => array(
+      'type' => 'models/Extension',
+    ),
+  );
     
 }

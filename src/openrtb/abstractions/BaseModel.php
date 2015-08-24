45223b83a4ac810c83cebf963e12717efb0f777c
Chris Beck <cwbeck@gmail.com>
2015-08-24 12:06:01 +0000

    updates

--- src/openrtb/abstractions/BaseModel.php
+++ src/openrtb/abstractions/BaseModel.php
@@ -34,7 +34,7 @@ abstract class BaseModel {
     if($data){
       foreach($this->attributes as $attr=>$opts) {
         if(!property_exists($data, $attr)) {
-          if(isset($opts['required'])) {
+          if(isset($opts['required']) && ($opts['required'] === true)) {
             throw new \openrtb\exceptions\ValidationException('Missing required attribute "' . $attr . '" for model '.$this->modelName.'.'); 
           } elseif(isset($opts['default_value'])) {
             $this->set($attr, $opts['default_value']);

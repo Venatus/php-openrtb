2bd0bf42541e67a030a931dca2dfb408b9f34cce
MichelVenatus <michel.bartz@venatusmedia.com>
2015-08-27 09:59:28 +0100

    Better bidder_url

--- tests/bidder.php
+++ tests/bidder.php
@@ -1,6 +1,4 @@
 <?php
-define('BIDDER_URL', 'http://bidder.alchemy.dev/');
-
 date_default_timezone_set('UTC');
 ini_set('default_charset','UTF-8');
 mb_internal_encoding('UTF-8');
@@ -9,6 +7,10 @@ require_once(__DIR__.'/../vendor/autoload.php');
 function addDelay() {
 	$delay = rand(10, 2000) * 1000; //Between 10ms and 2000ms, in microseconds for usleep()
 	usleep($delay);
+}
+
+function getBidderUrl() {
+	return 'http://' . $_SERVER['HTTP_HOST'] . '/';
 }
 
 //Get BidRequest
@@ -65,7 +67,7 @@ if(rand(1, 200) === 1 || $noImp) {
 		$priceInCents = rand(10, 400);
 		$floatPrice = $priceInCents / 100;
 		$bid->set('price', $floatPrice);
-		$bid->set('nurl', BIDDER_URL . 'win/' . $bidId);
+		$bid->set('nurl', getBidderUrl() . 'win/' . $bidId);
 		$bid->set('adm', file_get_contents(__DIR__ . '/vast_tags/tag_' . $i . '.xml'));
 
 		$seatBid->set('bid', [$bid]);
